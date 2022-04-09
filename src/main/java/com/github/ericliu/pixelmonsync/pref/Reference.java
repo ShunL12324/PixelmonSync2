2113bcb241d24adc4e27df2ee6c2df34176fa0c6
administrator <1169083089@qq.com>
2022-04-09 11:29:42 +0800

    Put perm nodes & strings to a new class.

---
+++ src/main/java/com/github/ericliu/pixelmonsync/pref/Reference.java
@@ -0,0 +1,17 @@
+package com.github.ericliu.pixelmonsync.pref;
+
+public class Reference {
+
+    public static final String PLUGIN_ID = "pixelmonsync";
+    public static final String PLUGIN_NAME = "Pixelmonsync";
+
+    public static final String USER_NODE = "user";
+    public static final String ADMIN_NODE = "admin";
+
+    public static final String PERM_NODE_LOAD = PLUGIN_ID + "." + ADMIN_NODE + "." + "load";
+    public static final String PERM_NODE_SAVE = PLUGIN_ID + "." + ADMIN_NODE + "." + "save";
+    public static final String PERM_NODE_SERVER = PLUGIN_ID + "." + ADMIN_NODE + "." + "server";
+    public static final String PERM_NODE_MIGRATE = PLUGIN_ID + "." + ADMIN_NODE + "." + "migrate";
+
+
+}
